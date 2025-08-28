01920c9c01f2e6099a2e775516912c3e68c10fb9
Jeremiah Saunders <46662314+UCYT5040@users.noreply.github.com>
2025-08-28 11:06:29 +0000

    Remove use of users API as it now requires auth, ask for user ID instead

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,90 +1,120 @@
 <script lang="ts">
-    import type {User} from './+page.server';
-    import UserSelect from '$lib/components/UserSelect.svelte';
-    import type {PageProps} from '../../.svelte-kit/types/src/routes/$types';
-    import PriceMap from '$lib/components/PriceMap.svelte';
+	import PriceMap from '$lib/components/PriceMap.svelte';
 
-    let {data}: PageProps = $props();
-
-    let selectedUser = $state<User | null>(null);
-    let item = $state('');
-    let itemChosen = $state(false);
+	let userId = $state('');
+	let userChosen = $state(false);
+	let item = $state('');
+	let itemChosen = $state(false);
 </script>
 
-<div class="flex flex-col items-center justify-center h-screen">
-    <h1 class="text-4xl font-bold mb-4">Ship Loot</h1>
-    <p class="text-md text-gray-700 mb-8">
-        Shop price calculator for <a class="text-blue-500 hover:underline" href="https://shipwrecked.hackclub.com/"
-                                     rel="noopener noreferrer" target="_blank" title="Shipwrecked">Hack Club's
-        Shipwrecked</a>.
-    </p>
-    <h2 class="text-2xl font-semibold mb-4">Select a user</h2>
-    {#if selectedUser}
-        <p class="text-lg mb-1">
-            Selected user: <span class="font-bold">{selectedUser.name}</span>
-        </p>
-        <button class="text-red-500 hover:underline mb-4" onclick={() => selectedUser = null}>
-            Go back
-        </button>
-    {:else}
-        <UserSelect
-                users={data.users}
-                onSelect={(user: User) => {
-                selectedUser = user;
-            }}
-        />
-    {/if}
-    <h2 class="text-2xl font-semibold mb-4">And an item</h2>
-    {#if itemChosen}
-        <p class="text-lg mb-2">
-            Selected item ID: <span class="font-bold">{item}</span>
-        </p>
-        <button
-                class="text-red-500 hover:underline mb-4"
-                onclick={() => {
-                item = '';
-                itemChosen = false;
-            }}
-        >
-            Go back
-        </button>
-    {:else}
-        <div class="border rounded-lg p-4 bg-white shadow-md w-128 mb-4">
-            <p class="text-lg mb-2">
-                Find the ID of your item <a href="https://shipwrecked.hackclub.com/api/bay/shop/items"
-                                            class="text-blue-500 hover:underline" target="_blank"
-                                            rel="noopener noreferrer" title="Shipwrecked API">here</a>. You'll need to
-                be logged in.
-            </p>
-            <p class="text-md mb-2">
-                <span class="font-semibold">Remember:</span>
-                Not all items have variable pricing.
-            </p>
-            <input
-                    type="text"
-                    placeholder="Enter item ID"
-                    bind:value={item}
-                    class="w-full p-2 border rounded mb-4"
-            />
-            <button
-                    class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-                    onclick={() => {
-                    if (item) {
-                        itemChosen = true;
-                    } else {
-                        alert('Please enter a valid item ID.');
-                    }
-                }}
-            >
-                Continue
-            </button>
-        </div>
-    {/if}
-    {#if selectedUser && itemChosen}
-        <h2 class="text-2xl font-semibold mb-4">Price Map</h2>
-        <p class="text-sm text-gray-600 mb-4">
-            Lower percentage = cheaper price.
-        </p>
-        <PriceMap userId={selectedUser.id} itemId={item}/>
-    {/if}
+<div class="flex h-screen flex-col items-center justify-center">
+	<h1 class="mb-4 text-4xl font-bold">Ship Loot</h1>
+	<p class="text-md mb-8 text-gray-700">
+		Shop price calculator for <a
+			class="text-blue-500 hover:underline"
+			href="https://shipwrecked.hackclub.com/"
+			rel="noopener noreferrer"
+			target="_blank"
+			title="Shipwrecked">Hack Club's Shipwrecked</a
+		>.
+	</p>
+	<h2 class="mb-4 text-2xl font-semibold">Enter your user ID</h2>
+	{#if userChosen}
+		<p class="mb-1 text-lg">
+			Selected user ID: <span class="font-bold">{userId}</span>
+		</p>
+		<button
+			class="mb-4 text-red-500 hover:underline"
+			onclick={() => {
+				userId = '';
+				userChosen = false;
+			}}
+		>
+			Go back
+		</button>
+	{:else}
+		<div class="mb-4 w-128 rounded-lg border bg-white p-4 shadow-md">
+			<p class="mb-2 text-lg">
+				Find your user ID <a
+					href="https://shipwrecked.hackclub.com/api/users/me"
+					class="text-blue-500 hover:underline"
+					target="_blank"
+					rel="noopener noreferrer"
+					title="Shipwrecked User API">here</a
+				>. You'll need to be logged in.
+			</p>
+			<input
+				type="text"
+				placeholder="Enter your user ID"
+				bind:value={userId}
+				class="mb-4 w-full rounded border p-2"
+			/>
+			<button
+				class="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+				onclick={() => {
+					if (userId) {
+						userChosen = true;
+					} else {
+						alert('Please enter a valid user ID.');
+					}
+				}}
+			>
+				Continue
+			</button>
+		</div>
+	{/if}
+	<h2 class="mb-4 text-2xl font-semibold">And an item</h2>
+	{#if itemChosen}
+		<p class="mb-2 text-lg">
+			Selected item ID: <span class="font-bold">{item}</span>
+		</p>
+		<button
+			class="mb-4 text-red-500 hover:underline"
+			onclick={() => {
+				item = '';
+				itemChosen = false;
+			}}
+		>
+			Go back
+		</button>
+	{:else}
+		<div class="mb-4 w-128 rounded-lg border bg-white p-4 shadow-md">
+			<p class="mb-2 text-lg">
+				Find the ID of your item <a
+					href="https://shipwrecked.hackclub.com/api/bay/shop/items"
+					class="text-blue-500 hover:underline"
+					target="_blank"
+					rel="noopener noreferrer"
+					title="Shipwrecked API">here</a
+				>. You'll need to be logged in.
+			</p>
+			<p class="text-md mb-2">
+				<span class="font-semibold">Remember:</span>
+				Not all items have variable pricing.
+			</p>
+			<input
+				type="text"
+				placeholder="Enter item ID"
+				bind:value={item}
+				class="mb-4 w-full rounded border p-2"
+			/>
+			<button
+				class="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+				onclick={() => {
+					if (item) {
+						itemChosen = true;
+					} else {
+						alert('Please enter a valid item ID.');
+					}
+				}}
+			>
+				Continue
+			</button>
+		</div>
+	{/if}
+	{#if userChosen && itemChosen}
+		<h2 class="mb-4 text-2xl font-semibold">Price Map</h2>
+		<p class="mb-4 text-sm text-gray-600">Lower percentage = cheaper price.</p>
+		<PriceMap {userId} itemId={item} />
+	{/if}
 </div>
